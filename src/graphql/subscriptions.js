04c4be2151c3e073f0c2a529122def3d0eaccf52
Mario Silva <marricardo@gmail.com>
2021-10-05 10:28:39 +0100

    Added Api and a way to get tasks

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTask = /* GraphQL */ `
+  subscription OnCreateTask {
+    onCreateTask {
+      id
+      title
+      description
+      Status
+      dueDate
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTask = /* GraphQL */ `
+  subscription OnUpdateTask {
+    onUpdateTask {
+      id
+      title
+      description
+      Status
+      dueDate
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTask = /* GraphQL */ `
+  subscription OnDeleteTask {
+    onDeleteTask {
+      id
+      title
+      description
+      Status
+      dueDate
+      createdAt
+      updatedAt
+    }
+  }
+`;
